3a79cf6082169c2adc39fb1180c8148f5962b217
Chetana Malakannavar <chetana.m@BRILLIO.COM>
2018-08-01 15:56:50 +0530

    updated today

--- loginapp/src/LoginForm.js
+++ loginapp/src/LoginForm.js
@@ -1,4 +1,70 @@
-//3rd try login
+//login using bind() and without using arrow function.
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { Link } from "react-router-dom";
+// import PropTypes from "prop-types";
+// import Account from './Account';
+
+// export default class LoginForm extends React.Component 
+// {
+//   static contextTypes = {
+//     router: PropTypes.object
+//   }
+
+//   constructor(props) 
+//   {
+//     super(props)
+//       this .handleSignIn=this.handleSignIn.bind(this)
+//        this.state={
+//                      error: false,
+//                   };
+
+//   }
+
+//   handleSignIn(e) 
+//   {
+//     e.preventDefault() 
+//       let username = this.refs.username.value
+//       let password = this.refs.password.value
+
+//       if(username === 'chetana@gmail.com' && password === 'abcdef')
+//     {   
+//         localStorage.setItem(username, 'chetana@gmail.com');
+//         localStorage.setItem(password, 'abcdef');
+//         console.log('valid');
+//         this.context.router.history.push('./Account');
+
+//     }
+
+//     else
+//     {
+//     this.setState({error: true});
+//     }   
+// }
+    
+// render() {
+
+
+  
+//     return  (
+//               <div className='container'>
+//               <form onSubmit={this.handleSignIn }>
+//               <h3>LOGIN</h3>
+//               <div><label> Username : </label> <input type="email" ref="username" placeholder="enter you username" required onChange={this.handleChange}/></div>
+//               <div><label> Password : </label><input type="password" ref="password" placeholder="enter password" required onChange={this.handleChange}/></div>
+//               <input type="submit" value="LOGIN" />
+//               {(this.state.error) && <span>Invalid credentials </span>}
+
+             
+//               </form>
+//               </div>
+//             )
+//         }
+
+// }
+
+//login without using bind() and with using arrow function.
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Link } from "react-router-dom";
@@ -7,20 +73,22 @@ import Account from './Account';
 
 export default class LoginForm extends React.Component 
 {
-  static contextTypes = {
-    router: PropTypes.object
-  }
-
-  constructor(props) 
+  constructor(props) //no need of binding event or function
   {
     super(props)
+      
        this.state={
                      error: false,
                   };
 
   }
 
-  handleSignIn(e) 
+  static contextTypes = {
+                            router: PropTypes.object
+                        }
+
+  //arrow function                  
+  handleSignIn =(e) =>
   {
     e.preventDefault() 
       let username = this.refs.username.value
@@ -40,14 +108,16 @@ export default class LoginForm extends React.Component
     this.setState({error: true});
     }   
 }
+
+ 
+
+ 
     
 render() {
-
-
   
     return  (
               <div className='container'>
-              <form onSubmit={this.handleSignIn.bind(this) }>
+              <form onSubmit={this.handleSignIn }> 
               <h3>LOGIN</h3>
               <div><label> Username : </label> <input type="email" ref="username" placeholder="enter you username" required onChange={this.handleChange}/></div>
               <div><label> Password : </label><input type="password" ref="password" placeholder="enter password" required onChange={this.handleChange}/></div>
@@ -61,4 +131,3 @@ render() {
         }
 
 }
-
